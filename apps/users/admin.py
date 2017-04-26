a1fe4dc680ec0364a0fc28ad9962608a92231a89
thrsky <1491474343@qq.com>
2017-04-26 15:58:22 +0800

    后台管理系统中增加user模块

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,14 @@
+# _*_ encoding:utf-8 _*_
 from django.contrib import admin
 
 # Register your models here.
+# 用于注册后台管理系统
+
+from .models import UserProfile
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(UserProfile,UserProfileAdmin)
