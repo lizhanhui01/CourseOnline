54ab08c821768947092c7c0a5102056650701579
thrsky <1491474343@qq.com>
2017-04-26 15:06:44 +0800

    把apps移动到apps中集中管理

--- CourseOnline/settings.py
+++ CourseOnline/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'apps.users'
 ]
 AUTH_USER_MODEL = "users.UserProfile"
 
