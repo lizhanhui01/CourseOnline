d5b32eaa1332302cb93803b78b1ed284a4443586
thrsky <1491474343@qq.com>
2017-04-26 15:57:28 +0800

    修复gender的長度問題

--- apps/users/models.py
+++ apps/users/models.py
@@ -15,7 +15,7 @@ class UserProfile(AbstractUser):
     # 继承原有的user属性之后  在下面加入我们自己特有的属性
     nick_name=models.CharField(max_length=50,verbose_name=u"昵称",default="")
     birday=models.DateField(validators=u"生日",null=True,blank=True)
-    gender=models.CharField(choices=(("male",u"男"),("famale","女")),default="female",max_length=6)
+    gender=models.CharField(choices=(("male",u"男"),("famale","女")),default="female",max_length=10)
     address=models.CharField(max_length=100,default="")
     mobile=models.CharField(max_length=11,null=True,blank=True)
     image=models.ImageField(upload_to="image/%Y/%m",default=u"image/default.png",max_length=100)
